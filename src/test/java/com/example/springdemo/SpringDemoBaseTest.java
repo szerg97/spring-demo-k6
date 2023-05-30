5cf525925070aff2606592281f63dfe5685fc5ba
Gergo Szalai <gergo.szalai@ericsson.com>
2023-05-30 16:45:37 +0200

    created contract for getting all samples

---
+++ src/test/java/com/example/springdemo/SpringDemoBaseTest.java
@@ -0,0 +1,30 @@
+package com.example.springdemo;
+
+import com.example.springdemo.controller.SampleController;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@DirtiesContext
+@AutoConfigureMessageVerifier
+public class BaseTestClass {
+    
+    @Autowired
+    private SampleController controller;
+    
+    @BeforeEach
+    public void setup(){
+        StandaloneMockMvcBuilder builder = MockMvcBuilders.standaloneSetup(controller);
+        RestAssuredMockMvc.standaloneSetup(builder);
+    }
+}
