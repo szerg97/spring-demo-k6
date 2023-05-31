7c5faabd1109331af04a743a015ff67521b5cb30
Gergo Szalai <gergo.szalai@ericsson.com>
2023-05-31 12:42:41 +0200

    modified shell script

--- src/test/k6/runK6Tests.sh
+++ src/test/k6/runK6Tests.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 JSON_RESULT=$1
+JSON_REPORT=report
 
 function cleanup(){
   rm -f ./${JSON_RESULT}.json
-  rm -f ./report.json
+  echo 'Deleted result: '${JSON_RESULT}'.json'
+  rm -f ./${JSON_REPORT}
+  echo 'Deleted report: '${JSON_REPORT}'.json'
 }
 
 function runTest(){
+  echo 'Running K6 tests... '
   k6 run --out json=${JSON_RESULT}.json script.js
+  echo 'Results can be found in '${JSON_RESULT}'.json'
+  echo 'Report can be found in '${JSON_REPORT}'.json'
 }
 
 cleanup
